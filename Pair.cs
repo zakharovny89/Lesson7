2d81d6f137bdb1a92c6e95c654ab27874cde8d21
zakharovny89 <zakharovny89@gmail.com>
2022-10-27 13:53:42 +0300

    Add project files.

---
+++ Pair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7
+{
+    internal class Pair<S, T>
+    {
+        internal S Key { get; set; }
+        internal T Value { get; set; }
+        internal Pair(S key, T value)
+        {
+            Key = key;
+            Value = value;  
+        }
+        internal void ShowPair()
+        {
+            Console.WriteLine($"Key = {Key}");
+            Console.WriteLine($"Value = {Value}");
+        }
+    }
+}
